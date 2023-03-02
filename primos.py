8e2964bdb32d95790b0b58add9c09d5e9f2f278c
Alex Mata Barrero <alex.mata@estudiantat.upc.edu>
2023-03-02 18:51:22 +0100

    canvis xulos

---
+++ primos.py
@@ -0,0 +1,21 @@
+"""
+Àlex Mata Barrero
+
+Mòdul de gestió de nombres primers
+"""
+
+def esPrimo(numero):
+    """
+    Devuelve True si su argumento es primo, y False si no lo es.
+    """
+    
+    for prova in range(2, numero)
+        if numero % prova == 0
+            return False
+
+
+
+    return True
+
+import doctest
+doctest.testmod()
